3e81766969bcd2eb314c1da9233a441b47125e61
kadir <develioglu306@gmail.com>
2022-04-12 22:32:44 +0300

    Last changes

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,16 +1,25 @@
-// import Splash from 'components/splash';
-import React from 'react';
+import Splash from 'components/splash';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import style from './home.module.scss';
 export default function Home() {
   document.title = 'Kaf Life';
+  const [load, setLoad] = useState(true);
   const redirect = useSelector((state) => state.authReducer.redirect);
-  return (
-    <iframe
-      src={redirect}
-      frameborder='0'
-      title='homepage'
-      className={style.frame}
-    ></iframe>
-  );
+  useEffect(() => {
+    setTimeout(() => setLoad(false), 1000);
+  }, []);
+
+  {
+    return load ? (
+      <Splash />
+    ) : (
+      <iframe
+        src={redirect}
+        frameborder='0'
+        title='homepage'
+        className={style.frame}
+      ></iframe>
+    );
+  }
 }
